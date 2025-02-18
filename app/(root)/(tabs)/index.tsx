70cb9288ac0df70f93c1e04a7e330a1afb2a84aa
wabrocker <billbrocker@gmail.com>
2025-02-18 18:24:45 -0500

    Implement Setup Routes

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,19 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Link href="/sign-in">Sign In</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property 1</Link>
+    </View>
+  );
+}
