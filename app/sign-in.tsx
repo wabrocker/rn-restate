ff0cbccf3b9bbc54afc463b4ed773c3c91a5374d
wabrocker <billbrocker@gmail.com>
2025-02-19 07:13:26 -0500

    Develop signin screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,53 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
+  const handleLogin = () => {};
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+
+        <View className="px-2">
+          <Text className="text-base font-rubik text-center uppercase text-black-200">
+            Welcome to ReState
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-center text-black-300 mt-2">
+            Lets Get You Closer to {"\n"}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+
+          <Text className="text-lg font-rubik text-center text-black-200 mt-12">
+            Login to ReState with Google
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleLogin}
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-6 h-6"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
